5878d1b357861bc93bd5858bdaa23c456ece4f5a
Default user <m.mejiap@uniandes.edu.co>
2020-02-17 12:31:10 +0000

    Ex3.cpp

--- Homework 3/Ex3.cpp
+++ Homework 3/Ex3.cpp
@@ -9,6 +9,9 @@ using namespace std;
 //Method declaration.
 double direct_sampling(int N);
 double importance_sampling(int N);
+double hit_and_miss(int N);
+double stratified_sampling(int N);
+double partially_SS(int N);
 
 //Main.
 int main(){
@@ -22,6 +25,36 @@ int main(){
         cout<<"N = "<<N<<", error = "<<r-0.946997089979748<<", Time: "<<difftime(t2,t1)/CLOCKS_PER_SEC<<endl;
     }
     
+    cout<<"SS:"<<endl;
+    for(int i=2;i<=6;i++){
+        int N = pow(10,i);
+        clock_t t1, t2;
+        t1 = clock();
+        double r = stratified_sampling(N);
+        t2 = clock();
+        cout<<"N = "<<N<<", error = "<<r-0.946997089979748<<", Time: "<<difftime(t2,t1)/CLOCKS_PER_SEC<<endl;
+    }
+    
+    cout<<"H&M:"<<endl;
+    for(int i=2;i<=6;i++){
+        int N = pow(10,i);
+        clock_t t1, t2;
+        t1 = clock();
+        double r = hit_and_miss(N);
+        t2 = clock();
+        cout<<"N = "<<N<<", error = "<<r-0.946997089979748<<", Time: "<<difftime(t2,t1)/CLOCKS_PER_SEC<<endl;
+    }
+    
+    cout<<"PSS:"<<endl;
+    for(int i=2;i<=6;i++){
+        int N = pow(10,i);
+        clock_t t1, t2;
+        t1 = clock();
+        double r = partially_SS(N);
+        t2 = clock();
+        cout<<"N = "<<N<<", error = "<<r-0.946997089979748<<", Time: "<<difftime(t2,t1)/CLOCKS_PER_SEC<<endl;
+    }
+    
     cout<<"IS:"<<endl;
     for(int i=2;i<=6;i++){
         int N = pow(10,i);
@@ -31,6 +64,8 @@ int main(){
         t2 = clock();
         cout<<"N = "<<N<<", error = "<<r-0.946997089979748<<", Time: "<<difftime(t2,t1)/CLOCKS_PER_SEC<<endl;
     }
+    
+
     return 0; 
 }
 
@@ -41,7 +76,7 @@ double direct_sampling(int N){
         double x = 10 * (double(rand()) / double(RAND_MAX));
         sum += exp(x*log(2) - 2 - log(tgamma(x+1)));
     }
-    return double(10*sum/N);
+    return double(10*sum)/double(N);
 }
 
 double importance_sampling(int N){
@@ -53,5 +88,50 @@ double importance_sampling(int N){
         double g_g = exp(-(G_inv*log(2)));
         sum += f_g/g_g;
     }
-    return double(sum/N);
+    return double(sum)/double(N);
+}
+
+double hit_and_miss(int N){
+    double minx = 0;
+    double maxx = 10;
+    double miny = 0;
+    double maxy = 0.28798091427502787;
+    int n_hits = 0;
+    double Ve = (maxy-miny)*(maxx-minx);
+    for(int i=0; i<N; i++){
+        double rx = (maxx-minx) * (double(rand()) / double(RAND_MAX)) + minx;
+        double ry = (maxy-miny) * (double(rand()) / double(RAND_MAX)) + miny;
+        double f = exp(rx*log(2) - 2 - log(tgamma(rx+1)));
+        if(f-ry>0){
+            n_hits += 1;
+        }
+    }
+    return double(Ve)*double(n_hits)/double(N);
+}
+
+double stratified_sampling(int N){
+    double sum = 0;
+    for(int i=0; i<N; i++){
+        double mini = 10/N * i;
+        double maxi = 10/N * (i+1);
+        double r = (maxi-mini) * (double(rand()) / double(RAND_MAX)) + mini;
+        double f = exp(r*log(2) - 2 - log(tgamma(r+1)));
+        sum += f;
+    }
+    return (10/double(N))*sum;
+}
+
+double partially_SS(int N){
+    double sum = 0;
+    double points = N/100;
+    for(int i=0;i<100; i++){
+        double mini = 10/100 * i;
+        double maxi = 10/100 * (i+1);
+        for(int j=0; j<points; j++){
+            double r = (maxi-mini) * (double(rand()) / double(RAND_MAX)) + mini;
+            double f = exp(r*log(2) - 2 - log(tgamma(r+1)));
+            sum += f;
+        }
+    }
+    return (10/double(N))*sum;
 }
